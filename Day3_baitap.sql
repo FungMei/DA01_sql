2cea9ab92ca8557c2fa80c3dfc29436a2040af02
FungMei <166972170+FungMei@users.noreply.github.com>
2024-04-14 14:16:58 +0700

    Create Day3_baitap.sql

---
+++ Day3_baitap.sql
@@ -0,0 +1,50 @@
+__baitap1
+SELECT name FROM CITY
+WHERE population > 120000 
+AND countrycode = 'USA';
+__baitap2
+SELECT * FROM city
+WHERE countrycode = 'JPN';
+__baitap3
+SELECT city, state FROM station;
+__baitap4
+SELECT DISTINCT city FROM Station
+WHERE city LIKE 'a%' OR city LIKE 'e%' OR city LIKE 'i%' 
+OR city LIKE 'o%' OR city LIKE 'u%';
+__baitap5
+SELECT DISTINCT city FROM Station
+WHERE city LIKE '%a' OR city LIKE '%e' OR city LIKE '%i' 
+OR city LIKE '%o' OR city LIKE '%u';
+__baitap6
+SELECT DISTINCT city FROM station
+WHERE left(city,1) NOT IN ('a', 'e', 'i', 'o', 'u');
+__baitap7
+SELECT name FROM employee
+ORDER BY name ASC;
+__baitap8
+SELECT name FROM employee
+WHERE salary > 2000 AND months < 10
+ORDER BY employee_id ASC;
+__baitap9
+SELECT product_id FROM products
+WHERE low_fats = 'Y' AND recyclable = 'Y';
+__baitap10
+SELECT name FROM customer
+WHERE NOT id = 2;
+__baitap11
+SELECT name, population, continent FROM world
+WHERE continent >= 3000000 AND population >= 25000000;
+__baitap12
+SELECT viewer_id FROM views
+WHERE author_id >= 1
+ORDER BY viewer_id ASC
+__baitap13
+SELECT part, assembly_step
+FROM parts_assembly
+WHERE finish_date IS NULL;
+__baitap14
+SELECT * FROM lyft_drivers
+WHERE yearly_salary <= 30000 OR yearly_salary >= 70000;
+__baitap15
+SELECT advertising_channel FROM uber_advertising
+WHERE money_spent > 100000;
